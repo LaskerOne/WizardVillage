11b9838b3c23725a2cf18cb7bcc310dee29d4550
JoseAngelNRA <jose.angel@numrot.com>
2023-05-15 16:50:25 -0500

    - Se actualiza el VillageModel y el VillageDto para realizar el levantamiento de la Base de datos mediante Entity Framework

--- WizardVillageAPI/Models/DTO/VillageDto.cs
+++ WizardVillageAPI/Models/DTO/VillageDto.cs
@@ -4,14 +4,24 @@ namespace WizardVillageAPI.Models.DTO
 {
   public class VillageDto
   {
+    [Key]
     public int Id { get; set; }
 
     [Required]
     [MaxLength(30)]
     public string? Nombre { get; set; }
 
+    public string? Detalle { get; set; }
+
+    [Required]
+    public decimal Tarifa { get; set; }
+
     public int Ocupantes { get; set; }
 
-    public int MetrosCuadrados { get; set; }
+    public double MetrosCuadrados { get; set; }
+
+    public string ImagenUrl { get; set; }
+
+    public string GradoSatisfaccion { get; set; }
   }
 }
--- WizardVillageAPI/Models/VillageModel.cs
+++ WizardVillageAPI/Models/VillageModel.cs
@@ -1,9 +1,30 @@
-﻿namespace WizardVillageAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WizardVillageAPI.Models
 {
   public class VillageModel
   {
+    [Key]
     public int Id { get; set; }
-    public string Nombre { get; set; }
-    public DateTime? fechaCreacion { get; set; }
+
+    [Required]
+    public string? Nombre { get; set; }
+
+    public string? Detalle { get; set; }
+
+    [Required]
+    public decimal Tarifa { get; set; }
+
+    public int Ocupantes { get; set; }
+
+    public double MetrosCuadrados { get; set; }
+
+    public string ImagenUrl { get; set; }
+
+    public string GradoSatisfaccion { get; set; }
+
+    public DateTime? FechaCreacion { get; set; }
+
+    public DateTime FechaActualizacion { get; set; }
   }
 }
